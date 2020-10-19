f27b0a00d128fd2af5519e6632b6185604de5c02
miroslavuzunov <miroslaw.uzunow@abv.bg>
2020-10-19 18:35:41 +0300

    General references submenu added

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -1,18 +1,35 @@
 package controllers;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 public class AdminController {
+
     @FXML
-    private VBox leftPane;
+    private Pane fadeMain;
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private VBox genRefPane;
 
     @FXML
     private Button regMrtBtn;
+
+    @FXML
+    private VBox leftPane;
 
     @FXML
     private Button genRefBtn;
@@ -27,16 +44,28 @@ public class AdminController {
     private Button clientBtn;
 
     @FXML
-    private Label userStatus;
-
-    @FXML
     private Label todaysDate;
 
     @FXML
-    private AnchorPane mainPane;
+    private Label userStatus;
 
     @FXML
     private void exit() throws IOException {
         App.setRoot("/view/LoginPanel");
+    }
+
+    @FXML
+    private void handleClicks(ActionEvent event){
+       if(event.getSource()==genRefBtn){
+           mainPane.setDisable(true);
+           fadeMain.toFront();
+           genRefPane.toFront();
+        }
+    }
+
+    public void backToLeftHome(ActionEvent event) {
+        mainPane.setDisable(false);
+        mainPane.toFront();
+        leftPane.toFront();
     }
 }

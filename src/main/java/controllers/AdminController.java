ac6ec170d143782156ea6be48d6c5aaa13c28334
miroslavuzunov <miroslaw.uzunow@abv.bg>
2020-10-19 15:36:57 +0300

    Design change

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -2,8 +2,38 @@ package controllers;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 public class AdminController {
+    @FXML
+    private VBox leftPane;
+
+    @FXML
+    private Button regMrtBtn;
+
+    @FXML
+    private Button genRefBtn;
+
+    @FXML
+    private Button stockRefBtn;
+
+    @FXML
+    private Button regProductBtn;
+
+    @FXML
+    private Button clientBtn;
+
+    @FXML
+    private Label userStatus;
+
+    @FXML
+    private Label todaysDate;
+
+    @FXML
+    private AnchorPane mainPane;
 
     @FXML
     private void exit() throws IOException {

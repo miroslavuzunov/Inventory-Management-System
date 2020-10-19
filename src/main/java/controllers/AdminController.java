8ec4c20459ee260278a969f6b4c4de5576a9075c
miroslavuzunov <miroslaw.uzunow@abv.bg>
2020-10-19 12:09:40 +0300

    Login panel design

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -1,12 +1,17 @@
 package controllers;
-
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.input.MouseEvent;
 
-public class AdminController {
+public class AdminController extends App {
 
     @FXML
-    private void switchToPrimary() throws IOException {
+    private void login() throws IOException {
         App.setRoot("/view/AdminPanel");
+    }
+
+    public void openGit(MouseEvent mouseEvent) {
+        getHostServices().showDocument("https://github.com/miroslavuzunov/Inventory-Management-System");
     }
 }
